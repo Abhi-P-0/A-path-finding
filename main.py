71a0c016a6ce300e11a80a3a87837d8d9fb39202
Abhijeet Prajapati <abhijeetprajapati85@gmail.com>
2024-05-24 16:49:01 -0400

    setup initial window

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import pygame
+import math
+from queue import PriorityQueue
 
+WIDTH = 800
+HEIGHT = 800
 
 def main():
-    print("asoidhjasioudhj")
+    window = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('A* Path Finding')
 
 
 if __name__ == '__main__':
